5842e966abacc69b8d7aefa1a97eaeb985efa58c
yangbin1 <yangbin199191@163.com>
2019-07-24 17:49:38 +0800

    2

---
+++ src/main/java/com/yangbin1/mq/order/OrderConsumer.java
@@ -0,0 +1,60 @@
+package com.yangbin1.mq.order;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class OrderConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+
+        //1.创建DefaultMQPushConsumer
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("demo_consumer_group");
+
+        //2.nameSrvAddress
+        consumer.setNamesrvAddr("47.101.10.100:9876");
+
+        //设置消息拉取上限
+        consumer.setConsumeMessageBatchMaxSize(2);
+
+        //3.订阅主题
+        consumer.subscribe("Topic_Demo", "*");
+
+        //4.消息监听
+        consumer.setMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                //迭代消息信息
+                for (MessageExt msg : list) {
+                    try {
+                        //获取主题
+                        String topic = msg.getTopic();
+                        //获取标签
+                        String tags = msg.getTags();
+
+                        //获取消息
+                        String result = new String(msg.getBody(), RemotingHelper.DEFAULT_CHARSET);
+                        System.out.println("Consumer消费信息---topic:" + topic + "," + "tags:" + tags + "result:" + result);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                        //重试
+                        return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                    }
+                }
+                //消费成功
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //5.开启consumer
+        consumer.start();
+    }
+}
+
---
+++ src/main/java/com/yangbin1/mq/order/OrderProducer.java
@@ -0,0 +1,59 @@
+package com.yangbin1.mq.order;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * @ClassName: Producer
+ * @Auther: yangbin1
+ * @Date: 2019/7/22 15:53
+ * @Description:
+ */
+public class OrderProducer {
+
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
+
+        //1.创建DefaultMQProducer
+        DefaultMQProducer producer = new DefaultMQProducer("demo_producer_group");
+
+        //2.nameSrvAddress
+        producer.setNamesrvAddr("47.101.10.100:9876");
+
+        //3.开启producer
+        producer.start();
+
+        //4.创建消息对象
+        Message message = new Message("Topic_Demo","Tags","Keys_1","hello".getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+        //5.发送消息
+        // 第一个参数发送的消息，
+        // 第二个参数指定队列
+        // 指定队列下标
+        for (int i = 0; i < 5; i++) {
+            SendResult result = producer.send(message, new MessageQueueSelector() {
+                        @Override
+                        public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                            // 获取队列的下标
+                            Integer index = (Integer) arg;
+                            //获取对应下标的队列
+                            return mqs.get(index);
+                        }
+                    },
+                    0);
+            System.out.println(result);
+        }
+        //关闭
+        producer.shutdown();
+
+    }
+}
